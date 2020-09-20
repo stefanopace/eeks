1ae8a89439ea54f25cf0319e590ddf9408b83470
Stefano Pace <stefanopace01@gmail.com>
2020-09-20 22:34:58 +0200

    incuded config parser in lib/core

--- tests/functionals/navigation.test.js
+++ tests/functionals/navigation.test.js
@@ -1,13 +1,12 @@
 const automenu = require('../../lib/core/automenu.js');
-const { ConfigParser } = require('../../lib/adapters/json-parser.js');
 const { FakeChooser } = require('../doubles/fake-chooser.js');
 const { SpyRunner } = require('../doubles/spy-runner.js');
 const { OptionChoice } = require('../../lib/core/choices/option-choice.js');
 const { GoBackChoice } = require('../../lib/core/choices/go-back-choice.js');
 const { ExitChoice } = require('../../lib/core/choices/exit-choice.js');
 
-test('can navigate a simple menu', () => {
-    const config = {
+describe('Navigation into simple menu', () => {
+    const simpleMenuConfig = {
         "type": "list",
         "returns": "action",
         "options": ["Hello", "Exit"],
@@ -39,18 +38,34 @@ test('can navigate a simple menu', () => {
         ]
     };
     
-    const chooser = new FakeChooser([
-        new OptionChoice("Hello"),
-        new OptionChoice("World")
-    ]);
-
-    const runner = new SpyRunner();
-    const parser = new ConfigParser(runner, chooser);
-    const rootNode = parser.parseConfig(config, null);
-
-    automenu.execute(rootNode);
-
-    expect(runner.commandHistory[0].command).toEqual(['echo Hello World!']);
-    expect(runner.commandHistory[0].parameters).toEqual({action: 'Hello', name: 'World'});
-    expect(runner.commandHistory[0].type).toBe('run-and-show');
+    test('Can navigate trough all menu', () => {
+        const chooser = new FakeChooser([
+            new OptionChoice("Hello"),
+            new OptionChoice("World")
+        ]);
+    
+        const runner = new SpyRunner();
+    
+        automenu.execute(simpleMenuConfig, runner, chooser);
+    
+        expect(runner.commandHistory[0].command).toEqual(['echo Hello World!']);
+        expect(runner.commandHistory[0].parameters).toEqual({action: 'Hello', name: 'World'});
+        expect(runner.commandHistory[0].type).toBe('run-and-show');
+    });
+    
+    test('Can go back to change choice', () => {
+        const chooser = new FakeChooser([
+            new OptionChoice("Hello"),
+            new GoBackChoice(),
+            new OptionChoice("Exit")
+        ]);
+    
+        const runner = new SpyRunner();
+    
+        automenu.execute(simpleMenuConfig, runner, chooser);
+    
+        expect(runner.commandHistory[0].command).toEqual(['echo "Bye!"']);
+        expect(runner.commandHistory[0].parameters).toEqual({action: 'Exit'});
+        expect(runner.commandHistory[0].type).toBe('run-and-show');
+    });
 });
