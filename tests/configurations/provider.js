e3e8dcafc2d61d81b43a98df0a1ae7e375166896
Stefano Pace <stefanopace01@gmail.com>
2020-09-20 23:34:48 +0200

    removed more dumb tests

--- tests/configurations/provider.js
+++ tests/configurations/provider.js
@@ -4,10 +4,12 @@ const simpleListMenu = JSON.parse(fs.readFileSync('./tests/configurations/simple
 const simpleMenuWithParams = JSON.parse(fs.readFileSync('./tests/configurations/simple-menu-with-params.json', 'utf-8'));
 const simpleRecursiveMenu = JSON.parse(fs.readFileSync('./tests/configurations/simple-recursive-menu.json', 'utf-8'));
 const simpleDynamicMenu = JSON.parse(fs.readFileSync('./tests/configurations/simple-dynamic-menu.json', 'utf-8'));
+const recursiveMenuWithAllMatchers = JSON.parse(fs.readFileSync('./tests/configurations/recursive-with-all-matchers.json', 'utf-8'));
 
 module.exports = {
     simpleListMenu,
     simpleMenuWithParams,
     simpleRecursiveMenu,
-    simpleDynamicMenu
+    simpleDynamicMenu,
+    recursiveMenuWithAllMatchers
 }
