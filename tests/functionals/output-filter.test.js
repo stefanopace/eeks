25b3092e59ccb7d8aa3e9510ec272cdca968da24
Stefano Pace <stefanopace01@gmail.com>
2020-10-17 21:20:43 +0200

    added debugging to regex filter

--- tests/functionals/output-filter.test.js
+++ tests/functionals/output-filter.test.js
@@ -1,6 +1,7 @@
 const automenu = require('../../lib/core/automenu');
 const { FakeChooser } = require('../doubles/fake-chooser');
 const { SpyRunner } = require('../doubles/spy-runner');
+const { DummyDebugger } = require('../doubles/dummy-debugger');
 const { OptionChoice } = require('../../lib/core/choices/option-choice');
 const { GoBackChoice } = require('../../lib/core/choices/go-back-choice');
 const { ExitChoice } = require('../../lib/core/choices/exit-choice');
@@ -14,8 +15,9 @@ describe('Output filter', () => {
 		]);
 	
 		const runner = new SpyRunner();
+		const debug = new DummyDebugger(); 
 	
-		automenu.execute(configProvider.simpleListWithOutputFilter, runner, chooser);
+		automenu.execute(configProvider.simpleListWithOutputFilter, runner, chooser, debug);
 		
 		expect(runner.commandHistory[0].parameters).toEqual(
 			{"name": "Stefano"}
