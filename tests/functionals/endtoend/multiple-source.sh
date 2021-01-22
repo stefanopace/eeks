3f2fb5bc4de8941179cdbe30113c1aa811123f83
Stefano Pace <stefanopace01@gmail.com>
2021-01-22 17:52:47 +0100

    fully renamed script to eeks

--- tests/functionals/endtoend/multiple-source.sh
+++ tests/functionals/endtoend/multiple-source.sh
@@ -1,4 +1,4 @@
-if result=$(echo "2\n1" | izi --simple -c ./tests/functionals/endtoend/izi.json -e ./tests/functionals/endtoend/source-file1.sh ./tests/functionals/endtoend/source-file2.sh 2> /dev/null | sed 's/[^[:print:]]//g'); then
+if result=$(echo "2\n1" | eeks --simple -c ./tests/functionals/endtoend/izi.json -e ./tests/functionals/endtoend/source-file1.sh ./tests/functionals/endtoend/source-file2.sh 2> /dev/null | sed 's/[^[:print:]]//g'); then
 	if echo $result | grep -q -E "^\[JThree Six$"; then # [J is the clear screen escape command
 		exit 0
 	else
